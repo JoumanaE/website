d2e1e1333e8bc9aa8e0882e280f1e863b91784e9
Joumana11 <joumana.elalaoui@gmail.com>
2017-03-17 08:56:49 -0400

    changes

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
@@ -20,4 +20,3 @@ application = get_wsgi_application()
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
 
-application = Cling(get_wsgi_application())
