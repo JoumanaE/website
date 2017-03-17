223710dc0a6accd65c42bef477188cfe758cd741
Joumana11 <joumana.elalaoui@gmail.com>
2017-03-17 07:51:16 -0400

    initial commits

---
+++ introduction/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+#Create your views here. 
+
+
+class HomePageView(TemplateView):
+	def get(self, request, **kwargs):
+		return render(request, 'index.html', context=None)
+
+
+#About view
+
+class AboutPageView(TemplateView):
+	template_name = "about.html"
+
+class ProjectsPageView(TemplateView):
+	template_name = "projects.html"
