432debee19b741ad4afaab3191f710d83b39bf42
Joumana11 <joumana.elalaoui@gmail.com>
2017-07-12 16:16:14 -0500

    some files modified-still an issue with the urls

--- introduction/urls.py
+++ introduction/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from introduction import views
 
 urlpatterns = [
-	url(r'^', views.HomePageView.as_view()),
+	url(r'^$', views.HomePageView.as_view()),
 	url(r'^about/$', views.AboutPageView.as_view()),
 	url(r'^projects/$', views.ProjectsPageView.as_view()),
 
 ]
-
